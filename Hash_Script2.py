d7e6f2ab9503c8ab8bc5fb2880f7d2cb6d5ca8c4
DonnieMarco <63506913+DonnieMarco@users.noreply.github.com>
2020-05-15 07:32:34 +0100

    Update Hash_Script2.py
    Remove old dictionary and some spelling corrections in the comments

--- Hash_Script2.py
+++ Hash_Script2.py
@@ -1,11 +1,11 @@
 #! /usr/bin/env python3
 
-#This python script checks a SHA256 hash against the HASH known for the application.
-#IF the Hash is not known, the opportunity is given to add the hash and the application to the dictionary.
+# This python script checks a SHA256 hash against the HASH known for the application.
+# If the Hash is not known, the opportunity is given to add the hash and the application to the dictionary.
 
 
 
-#Application_Hash_Dictionary  = {'51ea006fac7c7cf88dda19133fd2e701d53a774d0f9a6f094178db40424e2bd1': 'Notepad++ 7.8.1', 'd713c41ed85b4fcad4a810ba46e9a7753b0e6e49ec88cf81138d03f89d2b5814': 'Kali Linux 2019.1', '6789cdd2054bccb1d623d0ff32e4246d4d4c49c21cec85a7d1ea5922b5075a55': 'HandBrake-1.3.0.dmg'}
+
 
 from Hash_JSON_Data import Application_Hash_Dictionary
 
